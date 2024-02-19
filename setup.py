0fbf95081d5d7eea98f77cb5b1dd93e41aef42fe
Alejandro <alejandrovalenzuela051@gmail.com>
2024-02-18 19:06:00 -0600

    Agregado setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="metodos",
+    version="0.1.0",
+    description="Distintos iteradores para aproximaciones de ecuaciones definidas en dos dimensiones",
+    author="Your Name",
+    author_email="alejandrovalenzuela051@gmail.com",
+    packages=["metodos"],
+)

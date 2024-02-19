10eb7c7cfa86c02c4565adf68ea3e255a0e40ec2
Alejandro <alejandrovalenzuela051@gmail.com>
2024-02-19 09:48:12 -0600

    Valores iniciales de metodo hibrido agregado

--- metodos/abiertos/hibrido.py
+++ metodos/abiertos/hibrido.py
@@ -1,6 +1,8 @@
+from collections.abc import Iterable
 from dataclasses import dataclass
 
-from metodos.tipos import IteracionABC
+from metodos.abiertos.newton import Newton
+from metodos.tipos import Funcion2d, IteracionABC
 
 
 @dataclass
@@ -17,3 +19,30 @@ class IteracionHibrido(IteracionABC):
 
     def obtener_error_relativo(self) -> float:
         return self.error_relativo
+
+
+class Hibrido:
+    iteracion_actual: int
+    iteracion_maxima: int
+    funcion: Funcion2d
+    derivada: Funcion2d
+    fila_anterior: IteracionABC
+    a_inicial: float
+    b_inicial: float
+
+    def __init__(
+        self,
+        funcion: Funcion2d,
+        derivada: Funcion2d,
+        a_inicial: float,
+        b_inicial: float,
+        iteracion_maxima: int,
+    ) -> None:
+        if not b_inicial > a_inicial:
+            raise Exception("Rango inicial debe ser [a,b] donde b > a")
+
+        self.funcion = funcion
+        self.derivada = derivada
+        self.iteracion_maxima = iteracion_maxima
+        self.a_inicial = a_inicial
+        self.b_inicial = b_inicial
